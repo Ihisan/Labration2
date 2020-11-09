bacb4c2f0c5305a0931cf3777ce8b90159e852cf
Ihisan <ihisan.bashe@student.kyh.se>
2020-11-09 16:42:27 +0100

    Deletkey

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,5 +1,10 @@
+import org.w3c.dom.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
-    Node root;
+    static Node root;
 
     class Node {
         Node left;
@@ -9,35 +14,72 @@ public class BinaryTree {
 
         Node(int key) {
             this.key = key;
+            this.left = null;
+            this.right = null;
         }
     }
 
-    public void insert(int key){
-        root = insertRec (root,key);
+        int value(Node root) {
+            int min = root.key;
+            while (root.left != null) {
+                min = root.left.key;
+                root = root.left;
+            }
+            return min;
+        }
+
+
+
+    public void insert(int key) {
+        root = insertRec (root, key);
 
     }
 
 
     public Node insertRec(Node tree, int key) {
-        if(tree == null){
+        if (tree == null) {
             tree = new Node (key);
             return tree;
         }
-        if (key < tree.key){
-            tree.left = insertRec (tree.left,key);
-        }
-        else if (key > tree.key){
-            tree.right = insertRec (tree.right,key);
+        if (key < tree.key) {
+            tree.left = insertRec (tree.left, key);
+        } else if (key > tree.key) {
+            tree.right = insertRec (tree.right, key);
         }
         return tree;
     }
+
+
+    void deleteKey(int key){
+        root = deleteRec(root, key);
+    }
+
+    Node deleteRec(Node tree, int key){
+        if (tree == null) return tree;
+        if (key<tree.key)
+            tree.left=deleteRec (tree.left,key);
+        else if(key > tree.key)
+            tree.right = deleteRec (tree.left,key);
+
+        else{
+            if(tree.left == null) return tree.right;
+            else if (tree.right == null) return tree.left;
+
+            tree.key = value (tree.right);
+
+            tree.right = deleteRec (tree.right,tree.key);
+        }
+        return tree;
+    }
+
+
 
     public void printInOrderLeft(Node tree){
         if(tree == null){
             return;
         }
         printInOrderLeft (tree.left);
-        System.out.println (tree.key);
+        System.out.print (tree.key + " ");
         printInOrderLeft (tree.right);
     }
     public void printInOrderLeft(){
@@ -55,4 +97,7 @@ public class BinaryTree {
         System.out.println (tree.key);
         printInOrderRight (tree.left);
     }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,49 @@ public class Main {
 
     public static void main(String[] args) {
         BubbleSort ob = new BubbleSort ();
+        BinaryTree leef = new BinaryTree ();
         int arr[] = {38,27,43,3,9,82,10};
         ob.bubbleSort(arr);
         System.out.println("Sorted array");
-        ob.printArray(arr);
+        /*ob.printArray(arr);
+        ob.insert (1);
+        ob.insert (10);
         ob.insert (9);
+        ob.insert (12);
+        ob.insert (14);
+        ob.insert (17);
+        ob.insert (4);
+        ob.insert (30);
+        //ob.printInOrderLeft ();
+        ob.deleteKey (3);
+        ob.printInOrderLeft ();
+        ob.printInOrderRight ();*/
+
+        leef.insert (1);
+        leef.insert (10);
+        leef.insert (9);
+        leef.insert (12);
+        leef.insert (14);
+        leef.insert (17);
+        leef.insert (4);
+        leef.insert (30);
+        System.out.println ("Inorder traversal of the given tree");
+        leef.printInOrderLeft ();
+        System.out.println ("\nDelete 1");
+        leef.deleteKey (1);
+        System.out.println ("Inorder traversal of the modified tree");
+        leef.printInOrderLeft ();
+        System.out.println ("\nDelete 12 ");
+        leef.deleteKey (12);
+        System.out.println ("Inorder after deleting 12");
+        leef.printInOrderLeft ();
+        System.out.println ("\nDelete 30");
+        leef.deleteKey (30);
+        System.out.println ("Inorder after deleting 30");
+        leef.printInOrderLeft ();
+
+
+
 
     }
 }
