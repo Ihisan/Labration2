cf3367c13d96b4fcd9b6d5c160b162f8d8e453ef
Ihisan <ihisan.bashe@student.kyh.se>
2020-11-09 13:48:33 +0100

    Kod

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        BubbleSort ob = new BubbleSort ();
+        int arr[] = {38,27,43,3,9,82,10};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
     }
 }
