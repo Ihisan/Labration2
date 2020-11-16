5fe057f44643f4df2ff93c616ae15fefb6ddf11d
Ihisan <ihisan.bashe@student.kyh.se>
2020-11-16 17:15:25 +0100

    Revert "Clean up code"
    This reverts commit f314236da9ef5052ebac77b4166fcce12f5e5c45.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,3 +1,21 @@
+/*
+1. Implementera bubble sort
+Er implementation skall kunna ta en array av heltal och efter sortering med bubble sort resultera i en sorterad array, t.ex. [3,6, 65,11, 5,1] -> [1, 3, 5, 7, 11, 65]
+=======
+2. Fortsätt med implementationen av ett binärt sökträd
+ a) Lägg till metoder för att ta bort ett element, tänk på fallen:
+    1. Noden har inga barn
+    2. Noden har ett barn
+    3. Noden har två barn
+    Tänk också på att ni kan behöva hantera fallet där det som skall tas bort ligger i rot-noden.
+    =======
+  b) Lägg till metoder för traversering
+    1. pre-order
+    2. in-order
+    3. reverse in-order
+    4. post-order
+    För att visa att traverseringen fungerar räcker det att skriva ut värdet i varje nod
+ */
 public class BubbleSort {
 
     public static void bubbleSort(int array[]) {
